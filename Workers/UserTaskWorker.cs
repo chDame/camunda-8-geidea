e6328308bf727f58633c7513f5accd5002055d06
Christophe <christophe.dame@camunda.com>
2023-10-09 16:32:17 +0200

    usertask worker is still completing the task. Workaround to be provided

---
+++ Workers/UserTaskWorker.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Nodes;
+using Zeebe.Client.Accelerator.Abstractions;
+using Zeebe.Client.Accelerator.Attributes;
+
+namespace tasklistDotNetReact.Services
+{
+  [JobType("io.camunda.zeebe:userTask")]
+  [Timeout(2592000000)]
+  public class UserTaskWorker : IAsyncZeebeWorker
+
+  {
+    public UserTaskWorker()
+    {
+    }
+
+    public async Task HandleJob(ZeebeJob job, CancellationToken cancellationToken)
+    {
+      Dictionary<string, string> headers = job.getCustomHeaders<Dictionary<string, string>>();
+      Console.WriteLine("User task : " + headers["io.camunda.zeebe:formKey"]);
+    }
+  }
+}
+
